2f0b036e8c99d8da7ca62a46adf6b962284b9bc3
Daniel Hudson <danielhudson@shinobicorp.com>
2015-10-13 11:45:27 +0100

    Why did it work on dev?

--- src/Repositories/Abstracts/EloquentRepositoryAbstract.php
+++ src/Repositories/Abstracts/EloquentRepositoryAbstract.php
@@ -3,7 +3,7 @@
 namespace Metrique\Plonk\Repositories\Abstracts;
 
 use Illuminate\Container\Container;
-use Metrique\Plonk\Repositories\Contracts\EloquentRepositoryInterface;
+use Metrique\Plonk\Repositories\Abstracts\EloquentRepositoryAbstractInterface;
 
 /**
  * The Abstract Repository provides default implementations of the methods defined
@@ -11,7 +11,7 @@ use Metrique\Plonk\Repositories\Contracts\EloquentRepositoryInterface;
  * to the right eloquent model based on the $modelClassName.
  * 
  */
-abstract class EloquentRepositoryAbstract implements EloquentRepositoryInterface {
+abstract class EloquentRepositoryAbstract implements EloquentRepositoryAbstractInterface {
     
     protected $app;
     protected $model;

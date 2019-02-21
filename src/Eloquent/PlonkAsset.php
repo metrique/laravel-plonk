2044c7910479a8efc761a21320cc35b24c518dc8
Daniel Hudson <danielhudson@shinobicorp.com>
2019-02-21 17:39:45 +0000

    Add URL prefix

--- src/Eloquent/PlonkAsset.php
+++ src/Eloquent/PlonkAsset.php
@@ -88,6 +88,7 @@ class PlonkAsset extends Model
         }
 
         return implode('/', [
+            config('plonk.url', ''),
             rtrim(config('plonk.input.paths.base'), '/'),
             str_limit($this->hash, 4),
             sprintf('%s-%s.%s', $this->hash, $select->name, $this->extension)
@@ -110,6 +111,7 @@ class PlonkAsset extends Model
         }
 
         return implode('/', [
+            config('plonk.url', ''),
             rtrim(config('plonk.input.paths.base'), '/'),
             str_limit($this->hash, 4),
             sprintf('%s-%s.%s', $this->hash, $select->name, $this->extension)
